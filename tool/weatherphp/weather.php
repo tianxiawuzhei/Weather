202e64622269376a2d2c64ed37c44c69cc2511ea
tianxiawuzhei <staff@staffdeMac-mini-2.local>
2016-03-22 17:23:10 +0800

    1、修改weather.php ，支持命令行执行传入参数。 2、上传一份上海的天气json数据。

--- tool/weatherphp/weather.php
+++ tool/weatherphp/weather.php
@@ -7,12 +7,37 @@
  * Time: 17:15
  */
 
-//访问天气数据https://api.heweather.com/x3/weather?cityid=城市ID&key=你的认证key的工具
-$ch = curl_init();
-$url = 'https://api.heweather.com/x3/weather?cityid=CN101020100&key=1108c8fb59cb4aa3a1a0e24df690ce46';
+/*
+ * 访问天气数据https://api.heweather.com/x3/weather?cityid=城市ID&key=你的认证key的工具
+ */
+
+if ($argc < 2) {
+    // ask for input
+    fwrite(STDOUT, "Enter your cityid: ");
+
+    // get input
+    $cityid = trim(fgets(STDIN));
+} else {
+//    echo './weahter.php --id CN101010300';
+    $longopts  = array(
+        "id:",     // Required value
+    );
+    $options = getopt(null, $longopts);
+    $cityid = $options["id"];
+}
+
+$cu = curl_init();
+$url = <<<EOD
+https://api.heweather.com/x3/weather?cityid=$cityid&key=1108c8fb59cb4aa3a1a0e24df690ce46
+EOD;
+
+echo "\n";
+echo "url = $url";
+echo "\n\n";
+
 // 执行HTTP请求
-curl_setopt($ch , CURLOPT_URL , $url);
-curl_setopt($ch , CURLOPT_SSL_VERIFYPEER , false);
-$res = curl_exec($ch);
+curl_setopt($cu , CURLOPT_URL , $url);
+curl_setopt($cu , CURLOPT_SSL_VERIFYPEER , false);
+$res = curl_exec($cu);
 echo $res;
 ?>
