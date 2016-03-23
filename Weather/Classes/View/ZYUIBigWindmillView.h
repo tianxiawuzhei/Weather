fd891905729120868acb641778e3d5bfc6493a01
tianxiawuzhei <staff@staffdeMac-mini-2.local>
2016-03-23 19:24:38 +0800

    添加风速风车View

---
+++ Weather/Classes/View/ZYUIBigWindmillView.h
@@ -0,0 +1,15 @@
+//
+//  ZYUIBigWindmillView.h
+//  Weather
+//
+//  Created by staff on 16/3/23.
+//  Copyright © 2016年 zyqiosexercise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZYUIBigWindmillView : UIView
+
+@property (nonatomic, assign) NSInteger windSpeed;
+
+@end
